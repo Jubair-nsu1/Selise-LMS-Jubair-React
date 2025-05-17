959f41a4bf56326a34c30de561f4394dec8ceef5
jubair421 <a.k.m.ahmed@northsouth.edu>
2025-05-17 16:07:35 +0600

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,33 +1,39 @@
-import React from 'react'
-import {Link, useNavigate} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">LMS</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <Link class="nav-link" to="/">Course Catalog</Link>
-            </li>
-            <li class="nav-item active">
-              <Link class="nav-link" to="/learning-history">Learning History</Link>
-            </li>
-            <li class="nav-item active">
-              <Link class="nav-link" to="/my-learning">My Learning</Link>
-            </li>
-            <li class="nav-item">
-                <Link class="nav-link" to="/my-profile">My Profile</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  )
-}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">LMS</Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-export default Navbar
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Course Catalog</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/learning-history">Learning History</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/my-learning">My Learning</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/my-profile">My Profile</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
